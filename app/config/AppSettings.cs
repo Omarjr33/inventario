da4e564f09fbb289dc1eec9cf9c5bb3797d9751b
Omarjr33 <omarjrcardona33@gmail.com>
2025-05-09 06:57:31 -0500

    feat: :sparkles: Configuración de la aplicación

---
+++ app/config/AppSettings.cs
@@ -0,0 +1,12 @@
+namespace SGI.Config
+{
+    public static class AppSettings
+    {
+        public static string ConnectionString = "Server=localhost;Database=sgi_campus;Uid=root;Pwd=Poderoso-33;";
+        
+        // Configuraciones adicionales de la aplicación
+        public static int NumeroPaginacion = 10;
+        public static bool MostrarRegistrosEliminados = false;
+        public static int TiempoEsperaConsultas = 30; // segundos
+    }
+}
